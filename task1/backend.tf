9874c53160e64d11931a9dc782d8419a78ff81a7
Elifzyilmaz <elifzehralala@gmail.com>
2025-06-24 22:08:15 +0300

    add task 1

---
+++ task1/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket = "elifzehra-terraform-state-2025"  # burada kendi bucket ismini yaz
+    key    = "terraform.tfstate"
+    region = "us-east-1"                      # bölgeyi buraya sabit yaz
+  }
+}
+
---
+++ task1/terraform.tfvars
@@ -0,0 +1,2 @@
+bucket_name = "elifzehra-terraform-state-2025"
+region      = "us-east-1"
